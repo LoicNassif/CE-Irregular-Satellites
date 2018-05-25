18bdfa82eeec2de2d3dba163be859d4fa6f09184
Loic Nassif <loic.nassif.lachapelle@mail.utoronto.ca>
2018-05-25 17:50:10 -0400

    added empty base python file

---
+++ swarms.py
@@ -0,0 +1,13 @@
+"""A detailed work-through of the model for collisional evolution of irregular 
+satellite swarms presented in the paper Kennedy G.M., Wyatt M. C., 2011, Monthly 
+Notices of the Royal Astronomical Society, 412, 2137.
+
+IN DEVELOPMENT"""
+
+
+#main()
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
