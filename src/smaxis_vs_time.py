50d0b60dd08a174eb354388f17b924d6fa195b8f
Loic Nassif <loic.nassif.lachapelle@mail.utoronto.ca>
2018-11-15 01:17:12 -0500

    added init mass vs optimal time flux

--- src/smaxis_vs_time.py
+++ src/smaxis_vs_time.py
@@ -11,17 +11,16 @@ def a_pl_comp(s, t):
     part2 = Qd**0.63 * s.rho * (s.Dmax/1000) * (s.M_pl/5.972e24)**0.24 * s.eta**4.13
     return (t  * part1 / part2)**(1./4.13)
 
-def Fth(s, t, a_pl):
+def mass_comp(s, t):
     Qd = s.computeQd(s.Dc)
-    part1Rcc = 1.3e7 * (s.M_init/5.972e24) * (s.M_s/1.989e30)**1.38 * s.f_vrel**2.27
-    part2Rcc = Qd**0.63 * s.rho * (s.Dmax/1000) * (s.M_pl/5.972e24)**0.24 * s.eta**4.13 * a_pl**4.13
-    Rcc = part1Rcc / part2Rcc
-    part1 = (1/3.9e-6) * (1/(s.rho * (s.d_pl/1.496e11)**2)) * (s.Dc/1000)**-0.9 * (s.Dmin*1e6)**-0.7
-    part2 = (s.M_init/5.972e24) / (1 + Rcc * t)
-    return part1 * part2
+    part1 = 1.3e7 * (s.M_s/1.989e30)**1.38 * s.f_vrel**2.27
+    part2 = (Qd**0.63 * s.rho * (s.Dmax/1000) * (s.M_pl/5.972e24)**0.24
+                * s.eta**4.13 * (s.a_pl/1.496e11)**4.13)
+    return ((1/t)  * part2 / part1)
+
 
 #main
-def main(swarm_argv, lamb, t):
+def main(swarm_argv, lamb, t, M0flag=False):
     M0 = swarm_argv[0]; Dt = swarm_argv[1]
     Dmax = swarm_argv[2]; L_s = swarm_argv[3]
     M_s = swarm_argv[4]; M_pl = swarm_argv[5]
@@ -31,37 +30,65 @@ def main(swarm_argv, lamb, t):
 
     semi_major = []
     fth = []
+    M0v_list = []
 
-    s = swarms.CollSwarm(M0, Dt, Dmax, L_s, M_s, M_pl,
-                            a_pl, R_pl, eta, Nstr, d_pl,
-                            rho=RHO, fQ=5, f_vrel=4/pi,
-                            correction=True, alpha=1.2)
-
-    for i in range(len(t)):
-        s.updateSwarm(t[i])
-
-        a_plv = a_pl_comp(s, t[i])
-        #T = s.computeT(L_s, a_plv)
-        #bnu = s.computeBmu(lamb, T)
-        s2 = swarms.CollSwarm(M0, Dt, Dmax, L_s, M_s, M_pl,
-                                a_plv, R_pl, eta, Nstr, d_pl,
+    if not M0flag:
+        s = swarms.CollSwarm(M0, Dt, Dmax, L_s, M_s, M_pl,
+                                a_pl, R_pl, eta, Nstr, d_pl,
                                 rho=RHO, fQ=5, f_vrel=4/pi,
                                 correction=True, alpha=1.2)
-        F_th = s2.computeFth(array([lamb]), swarm=True)
-        fth.append(F_th/1e-26)
-        semi_major.append(a_plv)
 
-        #print("time: "+str(t[i])+"\t semi-major: "+str(a_plv)+"\t thermal: "+str(F_th))
+        for i in range(len(t)):
+            s.updateSwarm(t[i])
 
+            a_plv = a_pl_comp(s, t[i])
+            #T = s.computeT(L_s, a_plv)
+            #bnu = s.computeBmu(lamb, T)
+            s2 = swarms.CollSwarm(M0, Dt, Dmax, L_s, M_s, M_pl,
+                                    a_plv, R_pl, eta, Nstr, d_pl,
+                                    rho=RHO, fQ=5, f_vrel=4/pi,
+                                    correction=True, alpha=1.2)
+            F_th = s2.computeFth(array([lamb]), swarm=True)
+            fth.append(F_th/1e-26)
+            semi_major.append(a_plv)
 
-    plt.figure(1)
-    plt.plot(t, semi_major)
-    plt.ticklabel_format(style='sci', axis='both', scilimits=(0,0))
-    plt.xlabel("time [yr]")
-    plt.ylabel("semi-major axis [au]")
-    plt.loglog()
-    plt.show()
+            #print("time: "+str(t[i])+"\t semi-major: "+str(a_plv)+"\t thermal: "+str(F_th))
+    else:
+        s = swarms.CollSwarm(M0, Dt, Dmax, L_s, M_s, M_pl,
+                                a_pl, R_pl, eta, Nstr, d_pl,
+                                rho=RHO, fQ=5, f_vrel=4/pi,
+                                correction=True, alpha=1.2)
 
+        for i in range(len(t)):
+            s.updateSwarm(t[i])
+
+            M0v = mass_comp(s, t[i])
+            #T = s.computeT(L_s, a_plv)
+            #bnu = s.computeBmu(lamb, T)
+            s2 = swarms.CollSwarm(M0v*5.972e24, Dt, Dmax, L_s, M_s, M_pl,
+                                    a_pl, R_pl, eta, Nstr, d_pl,
+                                    rho=RHO, fQ=5, f_vrel=4/pi,
+                                    correction=True, alpha=1.2)
+            F_th = s2.computeFth(array([lamb]), swarm=True)
+            fth.append(F_th/1e-26)
+            M0v_list.append(M0v)
+
+    if not M0flag:
+        plt.figure(1)
+        plt.plot(t, semi_major)
+        plt.ticklabel_format(style='sci', axis='both', scilimits=(0,0))
+        plt.xlabel("time [yr]")
+        plt.ylabel("semi-major axis [au]")
+        plt.loglog()
+        plt.show()
+    else:
+        plt.figure(1)
+        plt.plot(M0v_list, fth)
+        plt.ticklabel_format(style='sci', axis='both', scilimits=(0,0))
+        plt.xlabel("initial Mass [Earth Mass]")
+        plt.ylabel("F_th [Jy]")
+        plt.loglog()
+        plt.show()
     plt.figure(2)
     plt.plot(t, fth)
     plt.ticklabel_format(style='sci', axis='both', scilimits=(0,0))
@@ -79,4 +106,4 @@ def main(swarm_argv, lamb, t):
 #     lamb = 1e-4
 #
 #     argv = [M0, Dt, Dmax, L_s, M_s, M_pl, a_pl, R_pl, eta, Nstr, d_pl]
-#     main(argv, lamb, t)
+#     main(argv, lamb, t, True)
