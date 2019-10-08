860c1fb222f9c0c987ce151327adea13759913cf
Daniel Tamayo <tamayo.daniel@gmail.com>
2019-10-07 23:57:11 -0400

    fixes

--- jupyter_examples/swarms.py
+++ jupyter_examples/swarms.py
@@ -346,32 +346,34 @@ class CollSwarm:
     === Attributes ===
     M0: initial mass of the swarm [kg]
     Dt: transition particle size of the swarm [m]
-    Dmax: maximum particle size of the swarm [m]
+    Dmax: maximum particle size involved in the collisional cascade [m]
     eta: constant fraction of the Hill radius at which irregulars orbit
     rho: mass density of the swarm [kg/m^3]
     stranding: whether to account for stranding and update Dc as a function of time
     """
     Dt: float; Dmax: float; Nstr: float; Dc: float; swarm: SizeDistribution
     Dmin: float; eta: float; fQ: float; f_vrel: float; 
-    Rcc0: float; tnleft: float; M_init: float; stranding: bool; Dmin_min: float
+    Rcc0: float; tnleft: float; M0: float; stranding: bool; Dmin_min: float
 
-    def __init__(self, star, planet, M0, Dmax=1e5, Dt=100, eta=0.3, Q=0.1, rho=1000, fQ=5, Nstr=6, f_vrel=4/pi, stranding=False, alpha=1./1.2, Dmin_min = 0., age=0., qs=1.9, qg=1.7, C1=0.1, C2=-1.9):
+    def __init__(self, star, planet, M0, Dmax=1e5, Dt=100, eta=0.3, Q=0.1, g=0.32, rho=1000, fQ=5, Nstr=6, f_vrel=4/pi, stranding=False, alpha=1./1.2, Dmin_min = 0., age=0., qs=1.9, qg=1.7, C1=0.1, C2=-1.9):
         self.planet = planet; self.star = star
         self.Dt = Dt; self.Dmax = Dmax; self.Nstr = Nstr
         self.alpha = alpha; self.Dmin_min = Dmin_min
-        self.age = age; self.C1 = C1; self.C2 = C2
+        self.C1 = C1; self.C2 = C2
         self.eta = eta; self.rho = rho; self.Q = Q
-        self.fQ = fQ; self.f_vrel = f_vrel
-        self.M_init = M0
+        self.fQ = fQ; self.f_vrel = f_vrel; self.g = g
+        self.M0 = M0
         self.stranding = stranding 
         self.qs = qs; self.qg = qg
 
         # these quantities are calculated but never change
+        # Assumes that initially Dc=Dmax. Dc will evolve to smaller values if stranding=True
         self.Dmin = self.computeDmin()
         self.swarm = SizeDistribution(Dmin=self.Dmin, Dmax=self.Dmax, M0=M0, Dt=Dt, rho=rho, qs=qs, qg=qg) # needed to init Rcc0
-        self.Tcol0 = self.computeTcol(Mtot=M0, Dc=Dmax)
-        self.tnleft = self.computetnleft(self.Dmax)
-        self.updateSwarm(self.age)
+        self.age = 0 # Need age = 0 when computing Tcol0
+        self.Tcol0 = self.computeTcol()
+        self.tnleft = self.swarm.Ntot(self.Dmax/2., self.Dmax)/(self.Nstr/self.Tcol0) # K11 Eq 18
+        self.updateSwarm(age)
 
     def computeDmin(self):
         """Compute the minimum sized object in the distribution."""
@@ -380,40 +382,14 @@ class CollSwarm:
         denom  = 8.*pi*G*self.star.M*C*self.rho # factor of 2 from Burns to get Diameter rather than radius
         return max(num/denom*(self.star.M/self.planet.M)**(1./3.), self.Dmin_min)
 
-    def computeNtot(self, dlow=None, dhigh=None):
-        """Return the distribution's number of particles."""
-        return self.swarm.Ntot(dlow, dhigh)
-
-    def computen(self, D):
-        """TBA"""
-        a = self.swarm.n(D)
-        return a
-
-    def computeQd(self, D):
-        """Compute the strength of planetesimal of size D"""
-        return 0.1*self.rho*(D/KM)**1.26/self.fQ
-    
-    def computeAtot(self, dlow=None, dhigh=None, cap=True):
-        """Compute the distribution's surface area."""
-        if dlow is None:
-            dlow = self.computeDmin()
-        if dhigh is None:
-            dhigh = self.computeDc()
-        A = self.swarm.Atot(dlow, dhigh)
-        if cap:
-            R_H = self.planet.a * (self.planet.M / (3 * self.star.M))**(1./3.)
-            return min(A, pi * R_H**2)
-        else:
-            return A
-
     def computeTinc(self): # swarm does not have the contribution to temperature from internal energy planet has. Just calc from incident light
         return (self.star.L*(1.-self.Q)/(16*pi*SIG*self.planet.a**2))**(1./4.)
     
-    def computeFscat(self, lamb, g, planet=False, swarm=False, Fstar=None):
-        return computeFscat(self.star, lamb, g, self.Q, self.computeAtot(), self.planet.a)
+    def computeFscat(self, lamb, planet=False, swarm=False, Fstar=None):
+        return computeFscat(self.star, lamb, self.g, self.Q, self.computeAtot(), self.planet.a)
 
-    def computeCRscat(self, g):
-        return computeCRscat(g, self.Q, self.computeAtot(), self.planet.a)
+    def computeCRscat(self):
+        return computeCRscat(self.g, self.Q, self.computeAtot(), self.planet.a)
         
     def computeFthermal(self, lamb):
         return computeFthermal(lamb, self.computeAtot(), self.computeTinc(), self.star.d)
@@ -421,61 +397,78 @@ class CollSwarm:
     def computeCRthermal(self, lamb):
         return computeCRthermal(lamb, self.computeTinc(), self.star.T, self.computeAtot(), self.star.A)
 
-    def computeRCC(self, Mtot=None, Dc=None):
-        if Dc is None:
-            Dc = self.computeDc()
-        if Mtot is None:
-            Mtot = self.computeMtot(Dc)
-
-        vrel = self.computeVrel()
-        Xc = self.computeXc(Dc)
-        V = 2.*pi*0.866*(self.eta*self.planet.RH)**3
-
-        return 3./2.*(6.-3.*self.qg)/(3.*self.qg-5.)*vrel*self.C1*Xc**self.C2*Mtot/self.rho/Dc/V
-
-    def computeTcol(self, Mtot=None, Dc=None):
-        return 1./self.computeRCC(Mtot, Dc)
-
     def computeVrel(self):
         """Compute the mean relative velocity of collisions."""
         return (4/pi)*sqrt(G*self.planet.M/(self.planet.RH*self.eta))
    
-    def computeXc(self, Dc=None):
+    def computeQd(self, Dc):
+        """Compute the strength of planetesimal of size D"""
+        return 0.1*self.rho*(Dc/KM)**1.26/self.fQ
+    
+    def computeXc(self):
         """Computes the constant Xc for which objects of size XcDc can destroy
         objects of size Dc."""
-        if Dc is None:
-            Dc = self.computeDc()
+        Dc = self.computeDc()
         return (2*self.computeQd(Dc)/(self.computeVrel()**2))**(1/3)
-
+    
     def computeDc(self):
         """Compute the new Dc if we want to correct for stranding and if after stranding time."""
+        if (self.age == 0): # Need to check for age = 0 for chicken & egg prob when initializing Tcol0
+            return self.Dmax
+        
         if (self.stranding == False) or (self.age <= self.tnleft): 
             return self.Dmax
         else:
             a = (1 + 0.4*(self.age - self.tnleft)/self.tnleft)**(self.alpha)
             return self.Dmax/a
     
-    def computetnleft(self, Dc):
-        Nhalf = self.swarm.Ntot(Dc/2., Dc)
-        return Nhalf/(self.Nstr/self.Tcol0)
+    def computeRCC(self):
+        Xc = self.computeXc()
+        vrel = self.computeVrel()
+        V = 2.*pi*0.866*(self.eta*self.planet.RH)**3
+        Mtot = self.computeMtot()
+        Dc = self.computeDc()
 
-    def computeMtot(self, Dc=None):
+        return 3./2.*(6.-3.*self.qg)/(3.*self.qg-5.)*vrel*self.C1*Xc**self.C2*Mtot/self.rho/Dc/V
+
+    def computeTcol(self):
+        return 1./self.computeRCC()
+
+    def computeMtot(self):
         """Compute the total mass at a given time t."""
+        if (self.age == 0):
+            return self.M0
+        
         if (self.stranding == False) or (self.age <= self.tnleft):
-            return (self.M_init)/(1 + self.age/self.Tcol0)
+            return (self.M0)/(1 + self.age/self.Tcol0)
         else:
-            if Dc is None:
-                Dc = self.computeDc()
+            Dc = self.computeDc()
             numerator = (3*self.qg - 3)*self.Nstr*pi*self.rho
             denominator = 6*(2**(3*self.qg - 3) - 1) * (6 - 3*self.qg)
             A = numerator/denominator
             return A*Dc**3
+    
+    def computeNtot(self, dlow=None, dhigh=None):
+        """Return the distribution's number of particles."""
+        return self.swarm.Ntot(dlow, dhigh)
+
+    def computen(self, D):
+        """TBA"""
+        return self.swarm.n(D)
+
+    def computeAtot(self, dlow=None, dhigh=None, cap=True):
+        """Compute the distribution's surface area."""
+        A = self.swarm.Atot(dlow, dhigh)
+        if cap:
+            return min(A, pi * self.planet.RH**2)
+        else:
+            return A
 
     def updateSwarm(self, t):
         """Description TBD"""
         self.age = t
         self.swarm = SizeDistribution(Dmin=self.computeDmin(), Dmax=self.Dmax, Dc=self.computeDc(), M0=self.computeMtot(), Dt=self.Dt, rho=self.rho)
-
-    def computeaopt(self, t): # From our Eq 6 (factor to increase a by to reach Tcol = t)
+   
+    def computeaopt(self): # From our Eq 6 (factor to increase a by to reach Tcol = t)
         power = (1.- 2./3.*self.C2)/2.+3.# exact power \approx 4.13 in Kennedy paper. Use exact so that if we put planet at a=aopt, we actually get exact Tcol
-        return self.planet.a*(t/self.Tcol0)**(1/power)
+        return self.planet.a*(self.age/self.Tcol0)**(1/power)
